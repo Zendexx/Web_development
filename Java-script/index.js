d8337b48987166955b2f3507b0d430ad860b56a7
DUAL SHOCK <danielbryan62F@gmail.com>
2023-03-11 14:40:13 +0300

    added new changes

---
+++ Java-script/index.js
@@ -0,0 +1,43 @@
+//creating variables
+var name = "panda"
+let number = 23
+console.log(name)
+var panda
+console.log(panda)
+var distance = 1500000009090
+console.log(distance)
+// Operators
+// 1: Arithmetic
+// 2: Logical
+// 3: Bitwise
+// 4: Assignment
+// 5: String
+// 6: Comparison
+var first_name="my "
+var second_name = "name is Panda"
+var full_name= first_name + second_name
+console.log(full_name)
+
+var num1 = true
+var num2 = false
+
+// var x =20
+// if(x<10){
+    // code if condition is true
+    // console.log("number is less than ten")
+// }else{
+    // code if condition is false
+    // console.log("number is greater than 10")
+// }
+// if else if checks more than one condition
+
+var country = "congo"
+var age = 20
+if(country == "congo"){
+    // code if true
+    if(age>=18){
+        console.log("Accepted")
+    }else{
+        console.log("Denied")
+    }
+}
